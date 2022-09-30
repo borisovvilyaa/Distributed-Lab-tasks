342d0b13d7851990905b3d8bc6a0f57a4a0b5d95
borisovvilyaa <113841816+borisovvilyaa@users.noreply.github.com>
2022-09-30 19:52:23 +0300

    Added number of key options

--- Task01 Large numbers/main.py
+++ Task01 Large numbers/main.py
@@ -1,4 +1,13 @@
 # @author Borusov Illia
 # @Version 1.0
- 
 
+def print_options (bits):
+    for i in range(len(bits)):
+        print(2**bits[i])
+        print('\n')
+
+def main(): 
+    bits = [8,16,32,64,128,256,512,1024,2048,4096]
+    print_options(bits)
+
+main()
