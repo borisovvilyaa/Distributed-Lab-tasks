d9e16223da8d274fd139cc89e4846f883929696d
borisovvilyaa <113841816+borisovvilyaa@users.noreply.github.com>
2022-10-01 00:04:33 +0300

    fix bugs, optimized the code

--- Task01 Large numbers/main.py
+++ Task01 Large numbers/main.py
@@ -1,22 +1,22 @@
 # @author Borusov Illia
-# @Version 1.0
+# @Version 0.2
 import random
-
+import datetime
 
 # Key generation in a range
 # @param bits_range bit range
 # @return key generated in a specific range
 def generate(bits_range): 
     print("Key: ", random.randint(0, bits_range),"\n")
-
+   
 # Output number of key variants
 # @param bits given bit rates
 # @return number of key options
 def print_options (bits):
     for i in range(len(bits)):
-        print(bits[i],"- bit, Key options:", 2**bits[i])
-        print('\n')
-        generate(2**bits[i])
+        bits_options =  2**bits[i]
+        print(bits[i],"- bit, Key options:",bits_options)
+        generate(bits_options)
 
 # Enter function
 def main(): 
@@ -24,3 +24,8 @@ def main():
     print_options(bits)
 
 main()
+
+# Code snippet to show code execution time
+# start = datetime.datetime.now()
+# end = datetime.datetime.now()
+# print('totally time is ', end - start, '\n')
