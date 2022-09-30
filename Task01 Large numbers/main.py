267a96463933567dcf381fa8766e9545dd6b5dd6
borisovvilyaa <113841816+borisovvilyaa@users.noreply.github.com>
2022-09-30 20:12:16 +0300

    add generate keys

--- Task01 Large numbers/main.py
+++ Task01 Large numbers/main.py
@@ -1,11 +1,24 @@
 # @author Borusov Illia
 # @Version 1.0
+import random
 
+
+# Key generation in a range
+# @param bits_range bit range
+# @return key generated in a specific range
+def generate(bits_range): 
+    print("Key: ", random.randint(0, bits_range),"\n")
+
+# Output number of key variants
+# @param bits given bit rates
+# @return number of key options
 def print_options (bits):
     for i in range(len(bits)):
-        print(2**bits[i])
+        print(bits[i],"- bit, Key options:", 2**bits[i])
         print('\n')
+        generate(2**bits[i])
 
+# Enter function
 def main(): 
     bits = [8,16,32,64,128,256,512,1024,2048,4096]
     print_options(bits)
