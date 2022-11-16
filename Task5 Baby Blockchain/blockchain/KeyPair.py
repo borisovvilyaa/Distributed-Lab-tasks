5caebd371bf820e1c67eec55ff46226c6afa5147
borisovvilyaa <borisovvilyaa@gmail.com>
2022-11-16 16:25:19 +0200

    add task05_01

---
+++ Task5 Baby Blockchain/blockchain/KeyPair.py
@@ -0,0 +1,17 @@
+from random import randint
+
+class KeyPair:
+    def __init__(self) -> None:
+        pass
+
+    #use SHA-3
+    #@param message -> whose will be generete key
+    #@return hash massage
+    def genKeyPair():
+        generate = randint(0, 10)
+        return generate
+
+    #Print key 
+    #@param key massage
+    def printKeyPair(key):
+        print(f"Key: {key}")
---
+++ Task5 Baby Blockchain/main.py
@@ -0,0 +1,5 @@
+from blockchain.KeyPair import *
+
+key = KeyPair.genKeyPair("Send some people 3 BTC")
+
+KeyPair.printKeyPair(key)
