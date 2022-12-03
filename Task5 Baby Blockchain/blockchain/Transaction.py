a8691eee19ada750fdae33501d872ed5d5f4af2d
Borusov Illia <113841816+borisovvilyaa@users.noreply.github.com>
2022-12-03 18:28:28 +0200

    add Transaction

--- Task5 Baby Blockchain/blockchain/Transaction.py
+++ Task5 Baby Blockchain/blockchain/Transaction.py
@@ -1,5 +1,10 @@
+from random import randint
 class Transaction:
-    def __init__(transactionID, setOfOperations, nonce):
-        pass
+    def __init__(self, transactionID, setOfOperations):
+        self.nonce = randint(10000, 99999) # random number
+        self.transactionID = transactionID
+        self.setOfOperations = setOfOperations
+        
+    def printTransaction(self):
+        print(f"nonce: {self.nonce}\ntransaction id: {self.transactionID}\nsetOfOperations: {self.setOfOperations}")
     
-Transaction("", "", "")
