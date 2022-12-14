f459aeb7611c213ebdfaa602c6302d830810105b
Borusov Illia <113841816+borisovvilyaa@users.noreply.github.com>
2022-12-14 12:17:42 +0200

    add full v class blockchain

--- Task5 Baby Blockchain/blockchain/Blockchain.py
+++ Task5 Baby Blockchain/blockchain/Blockchain.py
@@ -1,18 +1,27 @@
-class BlockChain: 
+class BlockChain(object): 
     def  __init__(self, block):
         self.block = block
+        self.verifyBlocks = []
         BlockChain.verify(self)
+
     def addBlockToHistory(self):
         print("done, add new transaction")
+        block = self.block
+        self.verifyBlocks.append(block)
+        BlockChain.verifyblock(self)
         #add to json 
+    def verifyblock(self):
+        print ("\n",self.verifyBlocks[0][1],"\n") #examination, if block have #1 block 
     def verify(self):
-        
         for i in range(0,4):
             if self.block[i][0] == self.block[i-1][1]:
-                print("ok")
                 if self.block[i][2]['Sign'] == True: 
-                    if self.block[i][2]['Sign'] != self.block[i][2]['Sign']: 
-                        BlockChain.addBlockToHistory(self.block)
+                    if self.block[i][0] != self.block[i][1]: 
+                        print("ok")
+                        self.block[i][3]['coinDatabase'] = self.block[i][2]['send'] + self.block[i-1][3]['coinDatabase']
+                        print ("coinbase: ",self.block[i])
+                        BlockChain.addBlockToHistory(self)
+                        
                     
                     
             else:
@@ -25,7 +34,10 @@ block = [
         "aab3abb8a152dbfa2c1b73816a3404d207ac16ab",
         {
             "Sign": True,
-            "send": 2.05
+            "send": 24.05
+        },
+        {
+            "coinDatabase": 0
         }
     ],
     [
@@ -34,25 +46,33 @@ block = [
         {
             "Sign": True,
             "send": 2.05
+        },
+        {
+            "coinDatabase": 0
         }
     ]
     ,
     [
-        "aab3abb8a152dbfa2c1b73816a3404d207ac16ab",
         "ce0b3dc9ea8c6adad3b7ffe7acb153a2584c0c8d",
+        "aab3abb8a152dbfa2c1b73816a3404d207ac16ab",
         {
-            "Sign": False,
+            "Sign": True,
             "send": 6.05
+        },
+        {
+            "coinDatabase": 0
         }
     ],
     [
         "ce0b3dc9ea8c6adad3b7ffe7acb153a2584c0c8d",
         "ce0b3dc9ea8c6adad3b7ffe7acb153a2584c0c8d",
         {
-            "Sign": False,
-            "send": 6.05
+            "Sign": True,
+            "send": 12.05
+        },
+        {
+            "coinDatabase": 0
         }
     ]
-]
-#some pool
+]#some chain of block
 blockNew = BlockChain(block)
